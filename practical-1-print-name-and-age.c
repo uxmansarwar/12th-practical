b9c6d3d2ba196fd1d8ce8234716f371928f49fa2
uxmansarwar <uxmansrwr@gmail.com>
2024-06-17 23:53:17 -0700

    init

---
+++ practical-1-print-name-and-age.c
@@ -0,0 +1,12 @@
+// Practical #1
+
+#include <stdio.h>
+#include <conio.h>
+
+
+main(){
+	
+	printf("My name is Uxman Sarwar! \n");
+	printf("My age is 20.\n");
+
+}
---
+++ practical-2-age-calculate.c
@@ -0,0 +1,21 @@
+// Practical #2
+
+#include <stdio.h>
+#include <conio.h>
+
+
+int main(void){
+
+
+	int age, days, months;
+	
+	age = 12;
+	months = age*12;
+	days = age*365;
+	
+	
+	printf("Age in months = %d\n", months);
+	printf("Age in days = %d\n", days);	
+
+	return 0;
+}
